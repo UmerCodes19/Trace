1ddf7bdd3828d1e8a3d80b5461c105604ddc95ac
UmerCodes19 <umertheking19@gmail.com>
2026-04-26 16:32:25 +0500

    idk

--- lib/data/services/api_service.dart
+++ lib/data/services/api_service.dart
@@ -9,7 +9,7 @@ final apiServiceProvider = Provider<ApiService>((ref) {
 class ApiService {
   final Dio _dio = Dio(BaseOptions(
     // TODO: Replace with your actual Render API URL
-    baseUrl: 'https://trace-qybrkad5.b4a.run/api',
+    baseUrl: 'https://trace-self.vercel.app/api',
     connectTimeout: const Duration(seconds: 10),
     receiveTimeout: const Duration(seconds: 10),
   ));
